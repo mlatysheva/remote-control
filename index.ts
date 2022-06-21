68d2c092341818f0e79161a0b1b78198ed18f844
mlatysheva <mlatysheva@gmail.com>
2022-06-21 23:12:13 +0200

    feat: add initial command and args setup

--- index.ts
+++ index.ts
@@ -12,18 +12,61 @@ const wss = new WebSocketServer({
   port: 8080,
 });
 
-const socket = new WebSocket('ws:localhost:8080');
+// const socket = new WebSocket('ws:localhost:8080');
 
 wss.on('connection', ws => {
   ws.on('message', data => {
-    // data will contain mouse_position
-    // we need to extract mouse position
+    
     const { x, y } = robot.getMousePos();
     console.log('received: %s', data);
-    ws.send(`mouse_position ${x},${y}`);
+    const [ command, ...args ] = data.toString().split(' ');
+    console.log(`command is ${command}`);
+    const [a, b] = args.map((arg) => parseInt(arg));
+    switch (command) {
+      case ('mouse_up'): {
+        console.log('mouse_up');
+        robot.moveMouse(x, y - a);
+        break;
+      };
+      case ('mouse_down'): {
+        console.log('mouse_down');
+        robot.moveMouse(x, y + a);
+        break;
+      };
+      case ('mouse_left'): {
+        console.log('mouse_left');
+        robot.moveMouse(x - a, y);
+        break;
+      };
+      case ('mouse_right'): {
+        console.log('mouse_right');
+        robot.moveMouse(x + a, y);
+        break;
+      };
+      case ('mouse_position'): {
+        ws.send(`mouse_position ${x},${y}`);
+        break;
+      };
+      case ('draw_square'): {
+        ws.send(`draw_square is requested with ${x}, ${y}, ${a}`);
+        break;
+      };
+      case ('draw_rectangle'): {
+        ws.send(`draw_square is requested with ${x}, ${y}, ${a}, ${b}`);
+        break;
+      };
+      case ('draw_circle'): {
+        ws.send(`draw_square is requested with ${x}, ${y}, ${a}`);
+        break;
+      };
+      case ('prnt_scrn'): {
+        ws.send(`prnt_scrn is requested with ${x},${y} and 200px`);
+        break;
+      };
+    }
   });
-  const { x, y } = robot.getMousePos();
-  ws.send(`mouse_position is ${x},${y}`);
+  // const { x, y } = robot.getMousePos();
+  // ws.send(`mouse_position ${x},${y}`);
 });
 
 wss.on('close', () => {
